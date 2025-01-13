0f15db053e04ceb23bc01a7489712ad98d61e206
noahk <noah.krause.888@gmail.com>
2025-01-12 21:11:26 -0600

    Updated HorizontalArm class to match new drive system and work with RR 1.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/HorizontalArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/HorizontalArm.java
@@ -1,52 +1,119 @@
 package org.firstinspires.ftc.teamcode.utilities;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 public class HorizontalArm {
 
-    private final Servo handLeftServo, handRightServo, handRotateServo, armExtendServo;
+    private final Servo handLeftServo, handRightServo, handRotateServo;
+    private final DcMotor armMotor;
     private static final double openHandLeftPosition = 0;
     private static final double closeHandLeftPosition = 0.3;
     private static final double openHandRightPosition = 1;
     private static final double closeHandRightPosition = 0.7;
-    private static final double minimumPosition = 0.7;
-    private static final double maximumPosition = 0.3;
-    private static final double extensionRange = 18;
-    private static final double positionRange = 0.4;
+    private static final int defaultStartingPosition = 0;
+    private static final int defaultPower = 1;
+    private static final double diameterOffset = 0;
+    private static final double diameter = 1.5 + diameterOffset;
+    private static final double circumference = Math.PI * diameter;
+    private static final double minimumDistance = 0;
+    private static final double maximumDistance = 17;
+    private static final double countsPerRevolution = 537.689839572;
 
     public HorizontalArm(HardwareMap hardwareMap) {
         handLeftServo = hardwareMap.get(Servo.class, "Horiz_Left");
         handRightServo = hardwareMap.get(Servo.class, "Horiz_Right");
         handRotateServo = hardwareMap.get(Servo.class, "Horiz_Rotate");
-        armExtendServo = hardwareMap.get(Servo.class, "Horiz_Extend");
 
-        armExtendServo.setDirection(Servo.Direction.REVERSE);
+        armMotor = hardwareMap.get(DcMotor.class, "hArm");
+        armMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        armMotor.setTargetPosition(defaultStartingPosition);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setPower(defaultPower);
     }
 
-    public void openHand() {
-        handLeftServo.setPosition(openHandLeftPosition);
-        handRightServo.setPosition(openHandRightPosition);
+    public class moveToHeight implements Action {
+
+        private double desiredDistance;
+
+        public moveToHeight(double height) {
+            desiredDistance = height;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            if (desiredDistance < minimumDistance) {
+                desiredDistance = minimumDistance;
+            }
+            if (desiredDistance > maximumDistance) {
+                desiredDistance = maximumDistance;
+            }
+
+            double desiredRevolutions = desiredDistance / circumference;
+            double desiredPosition = desiredRevolutions * countsPerRevolution;
+
+            armMotor.setTargetPosition((int) Math.round(desiredPosition));
+            return false;
+        }
     }
 
-    public void closeHand() {
-        handLeftServo.setPosition(closeHandLeftPosition);
-        handRightServo.setPosition(closeHandRightPosition);
+    public Action moveToHeight(double height) {
+        return new HorizontalArm.moveToHeight(height);
     }
 
-    public void moveToExtensionDistance(double desiredExtension) {
-        double desiredPosition = maximumPosition + (positionRange * (desiredExtension / extensionRange));
-
-        if (desiredPosition > minimumPosition) { desiredPosition = minimumPosition; }
-        if (desiredPosition < maximumPosition) { desiredPosition = maximumPosition; }
-
-        armExtendServo.setPosition(desiredPosition);
-    }
-    
-    public void rotateHandUp() {
-        handRotateServo.setPosition(0.3);
+    public class openHand implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            handLeftServo.setPosition(openHandLeftPosition);
+            handRightServo.setPosition(openHandRightPosition);
+            return false;
+        }
     }
 
-    public void rotateHandDown() {
-        handRotateServo.setPosition(0);
+    public Action openHand() {
+        return new HorizontalArm.openHand();
+    }
+
+    public class closeHand implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            handLeftServo.setPosition(closeHandLeftPosition);
+            handRightServo.setPosition(closeHandRightPosition);
+            return false;
+        }
+    }
+
+    public Action closeHand() {
+        return new HorizontalArm.closeHand();
+    }
+
+    public class handUp implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            handRotateServo.setPosition(0.3);
+            return false;
+        }
+    }
+
+    public Action handUp() {
+        return new HorizontalArm.handUp();
+    }
+
+    public class handDown implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            handRotateServo.setPosition(0);
+            return false;
+        }
+    }
+
+    public Action handDown() {
+        return new HorizontalArm.handDown();
     }
 }
